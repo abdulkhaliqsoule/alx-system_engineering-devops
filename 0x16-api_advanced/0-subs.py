08eb9c5df8170e50fb7ca9959a9ec46bd80faa5e
Abdul Khaliq <abdulkhaliqsoule@gmail.com>
2023-05-11 18:51:30 +0000

    reddit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+queries the Reddit API and returns the number of subscribers
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """function that queries the Reddit
+    API and returns the number of subscribers"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": ",mclovin-bot/0.0.1"}
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as e:
+        if e.response.status_code == 404:
+            print("Subreddit '{}' not found".format(subreddit))
+        else:
+            print("An HTTP error occured: {}".format(e))
+        return 0
+    except requests.exceptions.RequestException as e:
+        print("A network error occured: {}".format(e))
+        return 0
+
+    data = response.json()
+    return data['data']['subscribers']
